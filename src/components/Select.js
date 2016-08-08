256a5753783ba7cfd5c60bdf1de7f375bd5f8f8a
Traveler59 <dmitry_alekseevich@mail.ru>
2016-08-08 20:22:03 +0300

    Доделаны стили

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,7 @@
 window.Select = React.createClass({
   render: function() {
     return <div>
-      <p>Продолжительность долгого отдыха</p>
+      <p className="note">Продолжительность долгого отдыха</p>
       <select onChange={this.props.onChange}>
         <option>10</option>
         <option>15</option>
